475760a8175bf790c09b8d5745e26557c337a0c4
debora88-bip <debora.silva88@fatec.sp.gov.br>
2024-10-24 10:38:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
   // Create a map instance and set the initial view coordinates and zoom level
   var map = L.map('map').setView([-23.51679978489825, -46.83546507677398], 13);
 
-
+var greenIcon = L.icon({
+    iconUrl: 'https://www.freeiconspng.com/thumbs/dog-png/dog-png-30.png',
+ 
+    iconSize:     [80, 60], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+  
   // Add a tile layer to the map from OpenStreetMap
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -13,4 +20,9 @@ document.addEventListener('DOMContentLoaded', function () {
   // Create a marker with popup and add it to the map
   var marker = L.marker([-23.51679978489825, -46.83546507677398]).addTo(map);
   marker.bindPopup("Estamos na Fatec").openPopup();
+
+    // Create a marker with popup and add it to the map
+ var marker2 = L.marker([-23.52026413037824, -46.83670586136634], {icon: greenIcon}).addTo(map);
+  marker2.bindPopup("Amada turma de DMD, eu me perdi aqui").openPopup();
+
 });
